1ee083a296798a8ff960b45ac33729290b579d74
eatwanderexplore <eatwanderexplore@gmail.com>
2024-03-23 15:28:51 +1300

    created commentsSlice to load comments

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -1,2 +1,40 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
+export const loadCommentsForPostId = createAsyncThunk(
+    'comments/loadCommentsForPostId',
+    async (id) => {
+        const response = await fetch(`${URL}/comments`);
+        const json = await response.json();
+        return json;
+    }
+);
+
+export const commentsSlice = createSlice({
+    name: 'comments',
+    initialState: {
+        isLoadingComments: false,
+        failedToLoadComments: false,
+        byPostId: {},
+    },
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+            .addCase(loadCommentsForPostId.pending, (state, action) => {
+                state.isLoadingComments = true;
+                state.failedToLoadComments = false;
+            })
+            .addCase(loadCommentsForPostId.fulfilled, (state, action) => {
+                state.isLoadingComments = false;
+                state.failedToLoadComments = false;
+                state.byPostId[action.payload.postId] = action.payload.comments;
+            })
+            .addCase(loadCommentsForPostId.rejected, (state, action) => {
+                state.isLoadingComments = false;
+                state.failedToLoadComments = true;
+            });
+    },
+});
+
+export const selectComments = (state) => state.comments.byPostId;
+export default commentsSlice.reducer;
+export const isLoadingComments = (state) => state.comments.isLoadingComments;
