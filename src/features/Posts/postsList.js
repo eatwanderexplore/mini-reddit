e23b441153e43dffba371dad200413a03ecf5ee7
eatwanderexplore <eatwanderexplore@gmail.com>
2024-03-26 11:09:48 +1300

    updated css styles

--- src/features/Posts/postsList.js
+++ src/features/Posts/postsList.js
@@ -15,13 +15,21 @@ const PostsList = ({ subreddit }) => {
     <div>
       {loading && <p>Loading...</p>}
       {!loading && (
+        <div className="post-container">
         <ul>
           {posts.map((post) => (
             <li key={post.id}>
-              <a href={post.url} target='_blank' rel='noopener noreferrer'>{post.title}</a>
+              <h3><a href={post.url} target='_blank' rel='noopener noreferrer'>{post.title}</a></h3>
+              {post.url && (post.url.includes(".jpg") || post.url.includes(".jpeg") || post.url.includes(".gif")) && ( // Check if post has an image URL
+              <div className="post-image-container">
+                <img src={post.url} alt="" className="post-image" />
+              </div>
+            )}
+              <p>{post.selftext}</p>
             </li>
           ))}
         </ul>
+        </div>
       )}
     </div>
   );
