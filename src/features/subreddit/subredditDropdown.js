57949a9f508bdbcd3d459528c27cf4d404bc8bcc
eatwanderexplore <eatwanderexplore@gmail.com>
2024-03-26 08:59:02 +1300

    style changes

--- src/features/subreddit/subredditDropdown.js
+++ src/features/subreddit/subredditDropdown.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import './subredditDropdown.css';
 
 const DropdownMenu = ({ onTopicSelect }) => {
   const [isOpen, setIsOpen] = useState(false);
