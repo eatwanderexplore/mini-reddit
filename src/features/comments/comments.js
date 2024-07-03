b2c92a74fc7157b04cdeea1b95e15c76aade64b3
eatwanderexplore <111604338+eatwanderexplore@users.noreply.github.com>
2024-07-03 14:18:03 +1200

    added some style to the comments

--- src/features/comments/comments.js
+++ src/features/comments/comments.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './comments.css';
 const Comments = (props) => {
     const { comment } = props;
   
     return (
-        <div>
+        <div className="comment-container">
             <p className="comment-author">{comment.author}: </p>
             <p className="comment-body">{comment.body}</p>
         </div>
